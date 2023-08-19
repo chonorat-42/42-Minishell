30ad8dfdc4cd9a528beb9933f495d8d11c278fbd
Flash <flash@Ubuntu.myguest.virtualbox.org>
2023-08-19 14:53:15 +0200

    echo 90% done, error case when echo "\'a", doesnt display the simple quote

--- Sources/Commands/built_in/echo.c
+++ Sources/Commands/built_in/echo.c
@@ -6,34 +6,43 @@
 /*   By: pgouasmi <pgouasmi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 11:35:26 by pgouasmi          #+#    #+#             */
-/*   Updated: 2023/08/18 18:19:46 by pgouasmi         ###   ########.fr       */
+/*   Updated: 2023/08/19 14:50:23 by pgouasmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../Includes/minishell.h"
 
-int are_quotes_closed(char *str, int c)
+int are_all_quotes_closed(char *str)
 {
-	size_t i = 0;
+	size_t i;
+	size_t j;
 
+	i = 0;
 	while (str[i])
 	{
-		if (str[i] == c)
-			return (1);
+		if ((str[i] == '\'' || str[i] == '\"'))
+		{
+			j = i + 1;
+			while (str[j] && str[j] != str[i])
+				j++;
+			if (!str[j])
+				return (0);
+			i = j;
+		}
 		i++;
 	}
-	return (0);
+	return (1);
 }
 
 char *get_echo_opt(char *str, size_t *i)
 {
-	size_t	j;
-	char	*result;
+	size_t j;
+	char *result;
 
 	if (str[(*i)] != '-')
 		return (NULL);
 	j = *i + 1;
-	while(str[j] != ' ')
+	while (str[j] != ' ')
 		j++;
 	result = ft_substr(str, (*i), j - (*i));
 	if (!result)
@@ -43,69 +52,89 @@ char *get_echo_opt(char *str, size_t *i)
 	return (result);
 }
 
-void	display_between_quotes(char *str, int c, size_t *i, int fd)
+char *get_between_quotes(char *str, int c, size_t *i)
 {
-	// (*i)++;
+	char *result;
+	int j;
+
+	j = *i;
 	while (str[(*i)] && str[(*i)] != c)
-	{
-		write(fd, &str[(*i)], 1);
 		(*i)++;
-	}
+	result = ft_substr(str, j, *i - j);
+	if (!result)
+		return (NULL);
+	return (result);
+}
+
+char *get_other(char *str, size_t *i)
+{
+	size_t j;
+	char *temp;
+
+	j = *i;
+	while (str[j] && str[j] != '\'' && str[j] != '\"')
+		j++;
+	temp = ft_substr(str, (*i), j - (*i));
+	if (!temp)
+		return (NULL);
+	*i = j;
+	return (temp);
 }
 
 int echo_case(char *prompt, int fd)
 {
-	char *cmd;
 	char *option;
-	size_t	i;
+	size_t i;
+	char *to_display;
+	char *temp;
 
-	cmd = ft_substr(prompt, 0, 4);
-	// ft_printf("echo, cmd = %s\n\n", cmd);
 	i = 5;
 	option = get_echo_opt(prompt, &i);
 	if (option)
 	{
 		if (option[0] != '-' || ft_strlen(option) != 2 || option[1] != 'n')
-			return (ft_printf("Error\nWrong options for echo\n"), free(cmd), free(option), 0);
+			return (ft_printf("Error\nWrong options for echo\n"), free(option), 0);
 	}
-	while(prompt[i] && is_ws(prompt[i]))
+	while (prompt[i] && is_ws(prompt[i]))
 		i++;
-	// i++;
-	
-	
-	// ft_printf("after while is_ws : prompt[i] = %c\n\n", prompt[i]);
-
-	// ft_printf("after while is ws : &prompt[i] = %s\n\n", &prompt[i]);
-
-	
+	if (!are_all_quotes_closed(&prompt[i]))
+	{
+		if (option)
+			free(option);
+		return (ft_printf("Error\nUnclosed Quotes\n"), 0);
+	}
 	while (prompt[i])
 	{
-		if (prompt[i] != '\'' && prompt[i] != '\"')
-			write(fd, &prompt[i], 1);
-		else
+		if (prompt[i] == '\'' || prompt[i] == '\"')
 		{
-
-			// ft_printf("dans le else du while : prompt[i] = %c\n\n", prompt[i]);
-
-			
-			if (are_quotes_closed(&prompt[i], prompt[i]))
-				display_between_quotes(&prompt[i], prompt[i], &i, fd);
-			else
-			{
-				if (cmd)
-					free(cmd);
-				return(ft_printf("Error\nQuotes are not closed\n"), free(cmd), 12);
-			}
-		}
-		if (!prompt[i + 1] && (prompt[i] == '\'' || prompt[i + 1] == '\"'))
-			break;
-		if (prompt[i])
 			i++;
+			temp = get_between_quotes(prompt, prompt[i], &i);
+		}
+		else
+			temp = get_other(prompt, &i);
+		if (!temp)
+		{
+			if (option)
+				free(option);
+			return (1);
+		}
+		if (!to_display)
+			to_display = ft_strdup(temp);
+		else
+			to_display = ft_strjoin(to_display, temp);
+		free(temp);
+		if (!to_display)
+			return (free(option), 1);
 	}
-	if (!option)
-		write(1, "\n", 1);
+	if (to_display[0])
+	{
+		ft_putstr_fd(to_display, fd);
+		if (option)
+			write(fd, "$", 1);
+		write(fd, "\n", 1);
+	}
 	if (option)
 		free(option);
-	free(cmd);
+	free(to_display);
 	return (0);
 }
