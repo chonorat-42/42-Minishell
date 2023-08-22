1b4073f52d91cccc13a7fdd2c6294a10919928d1
Pascal Gouasmi <pgouasmi@z2r9p6.42lyon.fr>
2023-08-22 15:45:47 +0200

    expand functionnal but need to reorder the parsing. 1) expand, 2)quotes

--- Sources/Utils/tokenizer.c
+++ Sources/Utils/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: pgouasmi <pgouasmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:58:25 by pgouasmi          #+#    #+#             */
-/*   Updated: 2023/08/21 17:03:55 by pgouasmi         ###   ########.fr       */
+/*   Updated: 2023/08/22 15:43:14 by pgouasmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -222,71 +222,95 @@ char *expanse_envvar(char *str, char **envp)
 
 	size_t	i;
 	size_t	j;
-	char	*temp;
+	char	*bf_envvar;
+	char	*env_content;
 	char	*res;
+	char	*envvar;
+	int		envvar_index;
+	char	*temp;
+
+	bf_envvar = NULL;
+	env_content = NULL;
+	res = NULL;
+	envvar = NULL;
+	temp = NULL;
 
 	i = 0;
-	while (is_ws(str[i]))
-		i++;
-	j = i;
+	j = 0;
 	while (str[i])
 	{
 		if (content_has_envvar(&str[i]))
 		{
-			while (str[j] != '$')
-				j++;
-			temp = ft_substr(str, i, j - i);
-			ft_printf()
+			j = i;
+			i = find_char_index(&str[i], '$');
+			i += j;
+			bf_envvar = ft_substr(str, j, i - j);
+
+			if (res && bf_envvar)
+				res = ft_strjoin(res, bf_envvar);
+			else if (!res && bf_envvar)
+				res = ft_strdup(bf_envvar);
+			else if (!res && !bf_envvar)
+				res = NULL;
+			if (bf_envvar)
+				free(bf_envvar);
+
+			ft_printf("got before VAR : res = %s\n\n", res);
+
+			i++;
+			j = i;
+
+			ft_printf("should be at $+1, str[i] = %c\n\n", str[i]);
+
 			
+			while (str[i] && !is_ws(str[i]))
+				i++;
+			envvar = ft_substr(str, j, i - j);
+
+			ft_printf("envvar = %s\n\n", envvar);
+
+			envvar_index = find_envvar_index(envp, envvar);
+
+			env_content = get_envvar_content(envp[envvar_index], ft_strlen(envvar) + 1);
+
+			if (envvar)
+				free(envvar);
+
+			ft_printf("env_content = %s\n\n", env_content);
+
+			j = i;
+			if (res && env_content)
+				res = ft_strjoin(res, env_content);
+			else if (!res && env_content)
+				res = ft_strdup(env_content);
+			else if (bf_envvar && !env_content)
+				res = ft_strdup(bf_envvar);
+			else
+				res = NULL;
+			if (env_content)
+				free(env_content);
+			ft_printf("in if, res = %s\n\n", res);
+		}
+		else
+		{
+			while (str[i])
+				i++;
+			temp = ft_substr(str, j, i - j);
+
+			ft_printf("temp = %s\n\n", temp);
+			if (res && temp)
+				res = ft_strjoin(res, temp);
+			if (!res && temp)
+				res = ft_strdup(temp);
+			if (!res && !temp)
+				res = NULL;
+			if (temp)
+				free(temp);
+			ft_printf("in else, res = %s\n\n", res);
 		}
 	}
-	// size_t 	envvar_start;
-	// char 	*cmd;
-	// char	*envvar_content;
-	// int		var_index;
-	// char	*envvar;
-	// char	*res;
-	// char	*after;
-
-	// envvar_start = find_char_index(str, '$');
-	// cmd = ft_substr(str, 0, envvar_start);
-	// if (!cmd)
-	// 	return (NULL);
-	// envvar = get_envvar(&str[envvar_start + 1]);
-	// if (!envvar)
-	// 	return (free(cmd), NULL);
-	// var_index = find_envvar_index(envp, envvar);
-	// if (var_index == -1)
-	// {
-	// 	res = malloc(sizeof(char) * 1);
-	// 	res[0] = '\0';
-	// 	return (free(cmd), free(envvar), res);
-	// }
-	// else
-	// {
-	// if (var_index != -1)
-	// {
-	// 	envvar_content = get_envvar_content(envp[var_index], ft_strlen(envvar) + 1);
-	// 	if (!envvar_content)
-	// 		return (free(cmd), free(envvar), NULL);
-	// }
-	// else 
-	// 	envvar = NULL;
-	// if (envvar)
-	// {
-	// 	res = ft_strjoin(cmd, envvar_content);
-	// 	if (!res)
-	// 		return (free(cmd), free(envvar), NULL);
-	// }
-	// else
-	// 	res = ft_strdup(cmd);
-	// after = ft_substr(str, envvar_start + ft_strlen(envvar) + 1, ft_strlen(str));
-	// if (!after)
-	// 	return (free(cmd), free(envvar), free(res), NULL);
-	// res = ft_strjoin(res, after);
-	// if (!res)
-	// 	return (free(cmd), free(envvar), free(after), NULL);
-	// return (free(cmd), free(envvar_content), free(after), res);
+	ft_printf("res = %s\n\n", res);
+	return (res);
 }
 
 int create_token(t_mshell *shell, int i, int j)
@@ -336,6 +360,9 @@ int create_token(t_mshell *shell, int i, int j)
 	}
 	else
 		new->content = ft_strdup(result);
+	
+	ft_printf("\nin create token : new_content = %s\n", new->content);
+	
 	new->type = 0;
 	new->cmd_arr = NULL;
 	free(result);
