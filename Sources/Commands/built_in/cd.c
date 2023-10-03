5f03d5d357750f9e4c2e51155c687116e28a4732
Cedric Honorat <chonorat@z2r3p8.42lyon.fr>
2023-10-03 13:02:28 +0200

    cd now working

--- Sources/Commands/built_in/cd.c
+++ Sources/Commands/built_in/cd.c
@@ -14,44 +14,26 @@
 
 extern long long	g_status;
 
-char *get_cmd_arguments(char *prompt)
-{
-	char *result;
-	size_t i = 0;
-	size_t total_len = ft_strlen(prompt);
-	size_t res_len;
-	size_t j;
-
-	while (prompt[i] && prompt[i] != ' ')
-		i++;
-	j = total_len;
-	while (is_ws(prompt[j]))
-		j--;
-	res_len = j - i;
-	result = malloc(sizeof(char) * (res_len) + 1);
-	j = 0;
-	while (prompt[i])
-	{
-		result[j] = prompt[i];
-		i++;
-		j++;
-	}
-	result[j] = '\0';
-	return (result);
-}
-
 void cd_case(t_mshell *shell, char **cmd)
 {
 	char	*home;
+	int		arg_count;
 
-	if (count_arr_size(cmd) > 1)
+	arg_count = count_arr_size(cmd);
+	if (arg_count > 1)
+	{
+		if (arg_count > 2)
+			return (builtin_error(cmd[0], cmd[1], 3));
 		if (chdir(cmd[1]))
 			builtin_error(cmd[0], cmd[1], 1);
+	}
 	else
 	{
 		home = get_envvar_content(shell->envp, "HOME");
 		if (!home[0])
 			return (free(home), builtin_error(cmd[0], cmd[1], 2));
-		
+		if (chdir(home))
+			builtin_error(cmd[0], home, 1);
+		free(home);
 	}
 }
--- Sources/Utils/builtin_error.c
+++ Sources/Utils/builtin_error.c
@@ -6,7 +6,7 @@
 /*   By: chonorat <chonorat@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 13:17:36 by chonorat          #+#    #+#             */
-/*   Updated: 2023/10/02 18:36:37 by chonorat         ###   ########.fr       */
+/*   Updated: 2023/10/03 12:40:33 by chonorat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void	exit_error(char *arg, int error)
 		ft_dprintf(2, "%s: numeric argument required\n", arg);
 	else if (error == 2)
 	{
-		ft_printf("too many arguments\n");
+		ft_dprintf(2, "too many arguments\n");
 		g_status = 1;
 	}
 }
@@ -29,6 +29,11 @@ static void	cd_error(char *arg, int error)
 {
 	if (error == 1)
 		ft_dprintf(2, "%s: No such file or directory\n", arg);
+	if (error == 2)
+		ft_dprintf(2, "HOME not set\n");
+	if (error == 3)
+		ft_dprintf(2, "too many arguments\n");
+	g_status = 1;
 }
 
 void	builtin_error(char *cmd, char *arg, int error)
